2d9da3317ae90f16371ba4605cf4b11b2016b6d9
joe16273 <107936177+joe16273@users.noreply.github.com>
2022-07-18 14:59:36 -0400

    Update embedd.py

--- embedd.py
+++ embedd.py
@@ -1,6 +1,7 @@
 #Steps to get embedding:
-#1. Main function to compute embeddings of a batch of utterances is embed_utterance( in model.py for RTVC and in voice.encoder.py for Resemblyzer)
-# 2.Main functin 
+#1. Main function to compute embeddings of a batch of utterances is embed_utterance( in inference.py for RTVC and in voice.encoder.py for Resemblyzer)
+# 2.After the batch you need to gorup them in speakers and use ember speakers for that. (embed_speaker is not implemented for RTVC
+# and is in the voice.encoder.py file for resemblyzer)
 3.
 4.
 

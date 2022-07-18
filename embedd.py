4d2487df77a95c66dbf1e29958946797208b964a
joe16273 <107936177+joe16273@users.noreply.github.com>
2022-07-18 14:56:34 -0400

    Create embedd.py

---
+++ embedd.py
@@ -0,0 +1,7 @@
+#Steps to get embedding:
+#1. Main function to compute embeddings of a batch of utterances is embed_utterance( in model.py for RTVC and in voice.encoder.py for Resemblyzer)
+# 2.Main functin 
+3.
+4.
+
+  

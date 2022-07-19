cbbc8e1cca62081efdf5c3e5c40421ee2b42bdc4
joe16273 <107936177+joe16273@users.noreply.github.com>
2022-07-19 16:25:31 -0400

    Update training_pipeline.py

--- training_pipeline.py
+++ training_pipeline.py
@@ -1,1 +1,4 @@
-
+# First Step to train: Choose a dataset from VOXceleb, librispeech and call their respective pre_process functions from encoder_preprocess.py.
+# Second step: use the encoder_train.py with force_restart set to false and all the appropriate args. to cointinue training.
+#
+  
